273f09213386f1e2b00d3b474a4d8394f60fca8c
puretension <rlrlfhtm5@gmail.com>
2024-02-03 22:57:46 +0900

    Style: Color Set Setting

---
+++ src/style/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+  blue: '0EB5F9',
+  purple: 'C37CD2',
+  main: '14B4F9',
+  black: '000000',
+  subBlack: '343434',
+  font: '9F9F9F',
+  icon: 'B8CAD2',
+  subWhite: 'FAF4FC',
+  gray: 'F2F4F6',
+  componentColor: 'DDDDDD',
+  red: 'EF4452',
+  stroke: 'E5E5E5',
+};
+
+export default colors;

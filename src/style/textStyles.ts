5e93d6a0d834932f0ee25d892771b5b659d850d8
puretension <rlrlfhtm5@gmail.com>
2024-02-05 04:39:21 +0900

    Feat: Common TextStyle 셋팅

---
+++ src/style/textStyles.ts
@@ -0,0 +1,54 @@
+// styles.js
+import {StyleSheet} from 'react-native';
+import Colors from './primaryColors.ts';
+
+export const globalStyles = StyleSheet.create({
+  headline: {
+    fontFamily: 'Pretandard-Semibold',
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  title: {
+    fontFamily: 'Pretandard-Semibold',
+    fontSize: 20,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  bodyLargePrimary: {
+    fontFamily: 'Pretandard-Semibold',
+    fontSize: 18,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  bodyLargeSub: {
+    fontFamily: 'Pretandard-Regular',
+    fontSize: 18,
+    fontWeight: '300',
+    color: Colors.black,
+  },
+  bodyMediumPrimary: {
+    fontFamily: 'Pretandard-Semibold',
+    fontSize: 14,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  bodyMediumSub: {
+    fontFamily: 'Pretandard-Semibold',
+    fontSize: 14,
+    fontWeight: '300',
+    color: Colors.black,
+  },
+  labelPrimary: {
+    fontFamily: 'Pretandard-Bold',
+    fontSize: 12,
+    fontWeight: '700',
+    color: Colors.black,
+  },
+  labelSub: {
+    fontFamily: 'Pretandard-Regular',
+    fontSize: 12,
+    fontWeight: '400',
+    color: Colors.black,
+  },
+});

2f118ccfc4f4b7e931b218b4f8e5fde884c15663
puretension <rlrlfhtm5@gmail.com>
2024-02-05 04:38:31 +0900

    Style: color # 누락 추가

---
+++ src/style/primaryColors.ts
@@ -0,0 +1,18 @@
+const primaryColors = {
+  blue: '#0EB5F9',
+  buttonPressed: '#0D9FDA',
+  purple: '#C37CD2',
+  main: '#14B4F9',
+  black: '#000000',
+  subBlack: '#343434',
+  font: '#9F9F9F',
+  icon: '#B8CAD2',
+  subWhite: '#FAF4FC',
+  component: '#F2F4F6',
+  whiteGray: '#DDDDDD',
+  red: '#EF4452',
+  stroke: '#E5E5E5',
+  white: '#FFFFFF',
+};
+
+export default primaryColors;

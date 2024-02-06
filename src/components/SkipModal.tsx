fa0cd0a2e58afdf88943c6e67ee0a795e1746e00
puretension <rlrlfhtm5@gmail.com>
2024-02-07 04:56:44 +0900

    Feat: Skip용 Modal component 생성

---
+++ src/components/SkipModal.tsx
@@ -0,0 +1,94 @@
+// SkipModal.js
+import React from 'react';
+import {Modal, View, Text, Pressable, StyleSheet} from 'react-native';
+import SkipSvg from '../assets/images/skip.svg';
+import {globalStyles} from '../style/textStyles.ts';
+import primaryColors from '../style/primaryColors.ts'; // 경로는 실제 환경에 맞게 조정
+
+const SkipModal = ({isVisible, onClose, onSetNow}) => {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={isVisible}
+      onRequestClose={onClose}>
+      <View style={styles.overlayStyle}>
+        <View style={styles.modalView}>
+          <SkipSvg style={{paddingBottom: 80}} />
+          <Text style={[globalStyles.bodyLargePrimary, {textAlign: 'center'}]}>
+            {'취향을 설정하면 맞춤 팝업 정보를\n받아볼 수 있어요!'}
+          </Text>
+          <View style={{flexDirection: 'row', marginTop: 20}}>
+            <Pressable style={styles.buttonSkip} onPress={onClose}>
+              <Text
+                style={[
+                  globalStyles.bodyMediumSub,
+                  {color: primaryColors.font},
+                ]}>
+                건너뛰기
+              </Text>
+            </Pressable>
+            <Text
+              style={{
+                color: primaryColors.whiteGray,
+                alignSelf: 'center',
+                paddingHorizontal: 10,
+                paddingBottom: 8,
+                fontSize: 24,
+              }}>
+              |
+            </Text>
+            <Pressable onPress={onSetNow} style={styles.buttonSetNow}>
+              <Text
+                style={[
+                  globalStyles.bodyMediumSub,
+                  {color: primaryColors.blue},
+                ]}>
+                지금 설정하기
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  overlayStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    paddingHorizontal: 35,
+    paddingBottom: 15,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonSkip: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginRight: 10,
+  },
+  buttonSetNow: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginLeft: 10,
+  },
+});
+
+export default SkipModal;

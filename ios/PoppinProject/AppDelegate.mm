b4739688bfaf425dac7e35c01371e0625f2d6955
puretension <rlrlfhtm5@gmail.com>
2024-02-09 03:01:04 +0900

    Feat: FCM, Permissions(위치,사진,알림) Setting

--- ios/PoppinProject/AppDelegate.mm
+++ ios/PoppinProject/AppDelegate.mm
@@ -1,22 +1,61 @@
 #import "AppDelegate.h"
-
+#import "RNSplashScreen.h"
 #import <React/RCTBundleURLProvider.h>
-
 #import <RNKakaoLogins.h>
-
 #import <NaverThirdPartyLogin/NaverThirdPartyLoginConnection.h>
+#import <UserNotifications/UserNotifications.h> // Firebase 추가🚨
+#import <RNCPushNotificationIOS.h> // Firebase 추가🚨
+#import <Firebase.h> // Firebase 추가🚨
 
 @implementation AppDelegate
-
+// Required for the register event.
+ - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+ {
+  [RNCPushNotificationIOS didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+ }
+ // Required for the notification event. You must call the completion handler after handling the remote notification.
+ - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+ {
+   [RNCPushNotificationIOS didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+ }
+ // Required for the registrationError event.
+ - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+ {
+  [RNCPushNotificationIOS didFailToRegisterForRemoteNotificationsWithError:error];
+ }
+ // Required for localNotification event
+ - (void)userNotificationCenter:(UNUserNotificationCenter *)center
+ didReceiveNotificationResponse:(UNNotificationResponse *)response
+          withCompletionHandler:(void (^)(void))completionHandler
+ {
+   [RNCPushNotificationIOS didReceiveNotificationResponse:response];
+ }
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure]; // Firebase 추가🚨
   self.moduleName = @"PoppinProject";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
+   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
+    [super application:application didFinishLaunchingWithOptions:launchOptions];
+    [RNSplashScreen show];  // RN RNSplashScreen 할때 추가
+      // Define UNUserNotificationCenter // Firebase 추가🚨
+      UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+      center.delegate = self;
+
+      return YES; // 수정
+ }
+
+ //Called when a notification is delivered to a foreground app.
+  -(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:
+  (UNNotification *)notification withCompletionHandler:(void (^)
+  (UNNotificationPresentationOptions options))completionHandler
+  {
+    completionHandler(UNNotificationPresentationOptionSound |
+  UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionBadge);
+  }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {

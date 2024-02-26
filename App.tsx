31059a5784cd6757f794af3c431e0329edeae3ff
puretension <rlrlfhtm5@gmail.com>
2024-02-27 01:56:34 +0900

    Fix: No Apple Login in Android

--- App.tsx
+++ App.tsx
@@ -7,17 +7,26 @@ import {useEffect} from 'react';
 import appleAuth from '@invertase/react-native-apple-authentication';
 import store from './src/store';
 import LoadingScreen from './src/screens/splash/LoadingScreen.tsx';
+import {Alert, Platform} from 'react-native';
 
 // Provider 바깥에서는 useSelector 사용불가(따로 빼서 거기에 +)
 function App() {
   useEffect(() => {
     SplashScreen.hide(); // 앱 시작 시 SplashScreen 숨기기
     // onCredentialRevoked returns a function that will remove the event listener. useEffect will call this function when the component unmounts
-    return appleAuth.onCredentialRevoked(async () => {
-      console.warn(
-        'If this function executes, User Credentials have been Revoked',
-      );
-    });
+    // return appleAuth.onCredentialRevoked(async () => {
+    //   console.warn(
+    //     'If this function executes, User Credentials have been Revoked',
+    //   );
+    // });
+    if (Platform.OS === 'ios' && appleAuth.isSupported) {
+      // Apple 인증 실행 코드
+    } else {
+      // 지원하지 않는 경우의 처리 로직
+      Alert.alert('안내', '애플로그인 기능은 iOS 기기에서만 사용 가능합니다.');
+      console.log('애플 로그인은 iOS 13 이상에서만 지원됩니다.');
+    }
+
     // TODO localStorage, AsyncStorage 등을 사용하여 온보딩 완료 여부를 저장하고 조회하는 로직을 구현.
     // 예: AsyncStorage.getItem('onboardingComplete').then(value => setIsOnboardingComplete(!!value));
   }, []);
@@ -27,7 +36,7 @@ function App() {
     <Provider store={store}>
       <NavigationContainer>
         <AppInner />
-        <LoadingScreen />
+        {/*<LoadingScreen />*/}
       </NavigationContainer>
     </Provider>
   );

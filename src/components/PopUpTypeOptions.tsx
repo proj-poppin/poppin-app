ce21d64cc921edb3629e8d78edba819fd39b2b86
puretension <rlrlfhtm5@gmail.com>
2024-03-01 09:35:25 +0900

    Feat: 팝업 유형 선택 컴포넌트

---
+++ src/components/PopUpTypeOptions.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import OptionSingleButton from './OptionSingleButton.tsx';
+import OptionMultipleButton from './optionMultipleButton.tsx';
+
+const PopupTypeOptions = ({onSelectOption, selectedPopUpType}) => {
+  const options = [
+    {label: '🛍️ 소비형', value: '소비형'},
+    {label: '🖼️ 전시형', value: '전시형'},
+    {label: '🏃 체험형', value: '체험형'},
+  ];
+
+  return (
+    <View style={styles.container}>
+      {options.map((option, index) => (
+        <OptionMultipleButton
+          key={index}
+          id={index.toString()}
+          title={option.label}
+          onPress={() => onSelectOption(option.value)}
+        />
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginHorizontal: -5,
+  },
+});
+
+export default PopupTypeOptions;

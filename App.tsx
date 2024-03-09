4675fd745b49a8f06577ece40c2a3ea992e8c1fd
puretension <rlrlfhtm5@gmail.com>
2024-03-10 01:40:52 +0900

    Feat: 캘린더 Localization Korea 적용

--- App.tsx
+++ App.tsx
@@ -13,6 +13,54 @@ import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 
 // Provider 바깥에서는 useSelector 사용불가(따로 빼서 거기에 +)
+
+import {LocaleConfig} from 'react-native-calendars/src';
+
+// 한국어 설정
+LocaleConfig.locales.ko = {
+  monthNames: [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  monthNamesShort: [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  dayNames: [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+  ],
+  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+  today: '오늘',
+};
+
+LocaleConfig.defaultLocale = 'ko';
+
 function App() {
   useEffect(() => {
     SplashScreen.hide(); // 앱 시작 시 SplashScreen 숨기기

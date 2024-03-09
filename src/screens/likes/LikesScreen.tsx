d23b92f40b50ea55a9203f41c3bcb01754368eed
puretension <rlrlfhtm5@gmail.com>
2024-03-10 01:37:04 +0900

    Feat: 관심팝업 화면 UI 최종 구현(아마도..?)

--- src/screens/likes/LikesScreen.tsx
+++ src/screens/likes/LikesScreen.tsx
@@ -1,5 +1,11 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect, useState} from 'react';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
 import {globalStyles} from '../../style/textStyles.ts';
 import CalendarSvg from '../../assets/icons/calendar.svg';
 import primaryColors from '../../style/primaryColors.ts'; // Note: Imported 'primaryColors' twice, removed 'Colors'
@@ -18,22 +24,262 @@ import InterestSampleSvg from '../../assets/images/interestSample.svg';
 
 import DividerLine from '../../components/DividerLine.tsx';
 import InterestPopUpCard from '../../components/InterestPopUpCard.tsx';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../store/reducer.ts';
+import LoadingPoppinSvg from '../../assets/icons/loadingPoppin.svg';
+import NotLogginBox from '../../components/NotLogginBox.tsx';
+import ListIconSvg from '../../assets/icons/listIcon.svg';
+import {BottomSheetModal, BottomSheetModalProvider} from '@gorhom/bottom-sheet';
+import {Calendar} from 'react-native-calendars';
 
 function LikesScreen() {
   const [isLoading, setLoading] = useState(false);
+  // const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+  const [dates, setDates] = useState('');
+  const isLoggedIn = true;
+
+  // BottomSheetModal ref
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  // 바텀 시트를 여는 함수
+  const handlePresentModalPress = () => {
+    bottomSheetModalRef.current?.present();
+  };
+
+  // 달력에서 날짜 선택 시 처리 함수
+  const handleDayPress = day => {
+    setSelectedDate({
+      [day.dateString]: {selected: true, marked: true},
+    });
+    handlePresentModalPress(); // 달력에서 날짜 선택 후 바텀 시트 열기
+  };
+
+  // 달력 선택 날짜 상태
+  const [selectedDate, setSelectedDate] = useState({});
+  const [isCalendarView, setIsCalendarView] = useState(false);
+
+  const handleDateSelected = day => {
+    const newSelectedDate = {
+      [day.dateString]: {
+        selected: true,
+      },
+    };
+    setSelectedDate(day.dateString); // Store the selected date
+    bottomSheetModalRef.current?.present(); // Present the bottom sheet
+  };
+  const renderBottomSheetContent = () => (
+    <View style={styles.bottomSheetContent}>
+      <View style={styles.titleContainer}>
+        <Text style={globalStyles.title}>{selectedDate} Events</Text>
+      </View>
+      <InterestPopUpCard
+        Svg={InterestSampleSvg}
+        title="팝업 스토어 이름1"
+        date="2024.01.01-2024.02.02"
+        status={'운영 중'}
+      />
+      {/* Add more cards as needed */}
+    </View>
+  );
+
+  // Dynamically generate marked dates with the selected date marked in purple
+  const getMarkedDates = () => {
+    const today = new Date().toISOString().split('T')[0]; // Format today's date as YYYY-MM-DD
+
+    let markedDates = {
+      ...selectedDate, // Preserve previously selected dates
+      [today]: {
+        // Configuration for today's date
+        selectedDate: true, // Mark today as selected
+        textColor: 'black', // Ensuring text color is black for today
+        selectedColor: primaryColors.purpleLight, // Background color for today
+        dots: [
+          {color: primaryColors.blue, selectedDotColor: primaryColors.blue},
+          {color: primaryColors.purple, selectedDotColor: primaryColors.purple},
+          {color: primaryColors.white, selectedDotColor: primaryColors.white},
+        ],
+      },
+    };
+
+    // Ensure other dates also have their textColor set if needed
+    Object.keys(markedDates).forEach(date => {
+      if (date !== today) {
+        markedDates[date].textColor = markedDates[date].textColor || 'black'; // Default to black if not set
+      }
+    });
+
+    return markedDates;
+  };
+
+  const toggleView = () => {
+    setIsCalendarView(!isCalendarView);
+  };
   const dispatch = useAppDispatch();
   useEffect(() => {
-    // 로딩 시작
     dispatch(loadingSlice.actions.setLoading({isLoading: true}));
-    // 데이터 로딩 로직...
     setTimeout(() => {
-      // 로딩 완료 후 2초가 지나면 로딩 상태를 false로 변경
       dispatch(loadingSlice.actions.setLoading({isLoading: false}));
-    }, 6000);
-  }, [isLoading, dispatch]);
+    }, 2000);
+  }, [dispatch]);
 
-  return isLoading ? (
-    <LoadingScreen isLoading={isLoading} />
+  return isLoggedIn ? (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: primaryColors.white,
+        paddingHorizontal: 10,
+      }}>
+      <BottomSheetModalProvider>
+        <View style={styles.headerContainer}>
+          <Text style={globalStyles.headline}>관심 팝업</Text>
+          <TouchableOpacity
+            onPress={toggleView}
+            style={styles.calendarViewContainer}>
+            {isCalendarView ? (
+              <>
+                <Text style={styles.labelSmallBlue}>리스트 보기</Text>
+                <ListIconSvg />
+              </>
+            ) : (
+              <>
+                <Text style={styles.labelSmallBlue}>캘린더 보기</Text>
+                <CalendarSvg />
+              </>
+            )}
+          </TouchableOpacity>
+        </View>
+        {isCalendarView ? (
+          <View style={{flex: 1}}>
+            <Calendar
+              onDayPress={handleDateSelected}
+              markedDates={getMarkedDates()}
+              markingType="multi-dot"
+              theme={{
+                textDayHeaderFontWeight: '600',
+                textMonthFontWeight: '600',
+                todayButtonFontWeight: '600',
+                arrowColor: primaryColors.black,
+                backgroundColor: '#ffffff',
+                textSectionTitleColor: '#b6c1cd',
+                selectedDayBackgroundColor: primaryColors.purple,
+                todayTextColor: primaryColors.blue,
+                selectedDayTextColor: primaryColors.white,
+                dayTextColor: '#2d4150',
+                textDisabledColor: '#d9e1e8',
+                textDayFontSize: 16,
+                textDayFontWeight: '500',
+              }}
+            />
+            <BottomSheetModal
+              ref={bottomSheetModalRef}
+              index={1}
+              snapPoints={['25%', '40%', '75%']}
+              backgroundStyle={styles.bottomSheetBackground}
+              handleStyle={styles.bottomSheetHandle}>
+              {renderBottomSheetContent()}
+            </BottomSheetModal>
+          </View>
+        ) : (
+          <View style={{flex: 1}}>
+            <View style={styles.dropdownContainer}>
+              <CustomSelectDropdown
+                data={popUpTypes}
+                onSelect={(selectedItem, index) =>
+                  console.log(selectedItem, index)
+                }
+                buttonWidth={150}
+                iconComponent={<DownBlackSvg style={styles.dropdownIcon} />}
+                buttonTextAfterSelection={(selectedItem, index) => selectedItem}
+              />
+              <View style={{width: 100}} />
+              <CustomSelectDropdown
+                data={orderTypes}
+                onSelect={(selectedItem, index) =>
+                  console.log(selectedItem, index)
+                }
+                buttonWidth={100}
+                iconComponent={<OrderSvg style={styles.dropdownIcon} />}
+                buttonTextAfterSelection={(selectedItem, index) => selectedItem}
+                buttonTextStyle={globalStyles.labelPrimary}
+              />
+            </View>
+            <Text
+              style={[globalStyles.bodyLargePrimaryGray, styles.bodyContainer]}>
+              1월 15일
+            </Text>
+            {/*FlatList로 관심 팝업 모델객체 만들어서 후에 렌더링*/}
+            <DividerLine height={3} />
+            <InterestPopUpCard
+              Svg={InterestSampleSvg} // 필요한 경우 다른 SVG로 대체
+              title="팝업 스토어 이름1"
+              date="2024.01.01-2024.02.02"
+              status={'운영 중'}
+            />
+            <DividerLine height={3} />
+            <InterestPopUpCard
+              Svg={InterestSampleSvg} // 필요한 경우 다른 SVG로 대체
+              title="팝업 스토어 이름1"
+              date="2024.01.01-2024.02.02"
+              status={'운영 중'}
+            />
+            <DividerLine height={3} />
+            <InterestPopUpCard
+              Svg={InterestSampleSvg} // 필요한 경우 다른 SVG로 대체
+              title="팝업 스토어 이름1"
+              date="2024.01.01-2024.02.02"
+              status={'운영 중'}
+            />
+            <DividerLine height={3} />
+            <View />
+          </View>
+        )}
+        {/*<View style={styles.dropdownContainer}>*/}
+        {/*  <CustomSelectDropdown*/}
+        {/*    data={popUpTypes}*/}
+        {/*    onSelect={(selectedItem, index) => console.log(selectedItem, index)}*/}
+        {/*    buttonWidth={150}*/}
+        {/*    iconComponent={<DownBlackSvg style={styles.dropdownIcon} />}*/}
+        {/*    buttonTextAfterSelection={(selectedItem, index) => selectedItem}*/}
+        {/*  />*/}
+        {/*  <View style={{width: 100}} />*/}
+        {/*  <CustomSelectDropdown*/}
+        {/*    data={orderTypes}*/}
+        {/*    onSelect={(selectedItem, index) => console.log(selectedItem, index)}*/}
+        {/*    buttonWidth={100}*/}
+        {/*    iconComponent={<OrderSvg style={styles.dropdownIcon} />}*/}
+        {/*    buttonTextAfterSelection={(selectedItem, index) => selectedItem}*/}
+        {/*    buttonTextStyle={globalStyles.labelPrimary}*/}
+        {/*  />*/}
+        {/*</View>*/}
+        {/*<Text style={[globalStyles.bodyLargePrimaryGray, styles.bodyContainer]}>*/}
+        {/*  1월 15일*/}
+        {/*</Text>*/}
+        {/*/!*FlatList로 관심 팝업 모델객체 만들어서 후에 렌더링*!/*/}
+        {/*<DividerLine height={3} />*/}
+        {/*<InterestPopUpCard*/}
+        {/*  Svg={InterestSampleSvg} // 필요한 경우 다른 SVG로 대체*/}
+        {/*  title="팝업 스토어 이름1"*/}
+        {/*  date="2024.01.01-2024.02.02"*/}
+        {/*  status={'운영 중'}*/}
+        {/*/>*/}
+        {/*<DividerLine height={3} />*/}
+        {/*<InterestPopUpCard*/}
+        {/*  Svg={InterestSampleSvg} // 필요한 경우 다른 SVG로 대체*/}
+        {/*  title="팝업 스토어 이름1"*/}
+        {/*  date="2024.01.01-2024.02.02"*/}
+        {/*  status={'운영 중'}*/}
+        {/*/>*/}
+        {/*<DividerLine height={3} />*/}
+        {/*<InterestPopUpCard*/}
+        {/*  Svg={InterestSampleSvg} // 필요한 경우 다른 SVG로 대체*/}
+        {/*  title="팝업 스토어 이름1"*/}
+        {/*  date="2024.01.01-2024.02.02"*/}
+        {/*  status={'운영 중'}*/}
+        {/*/>*/}
+        {/*<DividerLine height={3} />*/}
+        {/*<View />*/}
+      </BottomSheetModalProvider>
+    </SafeAreaView>
   ) : (
     <SafeAreaView
       style={{
@@ -43,55 +289,19 @@ function LikesScreen() {
       }}>
       <View style={styles.headerContainer}>
         <Text style={globalStyles.headline}>관심 팝업</Text>
-        <View style={styles.calendarViewContainer}>
-          <Text style={styles.labelSmallBlue}>캘린더 보기</Text>
-          <CalendarSvg style={styles.calendarIcon} />
+      </View>
+      <View style={styles.center}>
+        <LoadingPoppinSvg />
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <NotLogginBox
+            text1={'로그인하고'}
+            text2={'관심 팝업을 저장해보세요!'}
+            buttonText={'로그인 하러 가기'}
+            onPress={() => console.log('로그인하러 가기')}
+            isNeedBox={false}
+          />
         </View>
       </View>
-      <View style={styles.dropdownContainer}>
-        <CustomSelectDropdown
-          data={popUpTypes}
-          onSelect={(selectedItem, index) => console.log(selectedItem, index)}
-          buttonWidth={150}
-          iconComponent={<DownBlackSvg style={styles.dropdownIcon} />}
-          buttonTextAfterSelection={(selectedItem, index) => selectedItem}
-        />
-        <View style={{width: 100}} />
-        <CustomSelectDropdown
-          data={orderTypes}
-          onSelect={(selectedItem, index) => console.log(selectedItem, index)}
-          buttonWidth={100}
-          iconComponent={<OrderSvg style={styles.dropdownIcon} />}
-          buttonTextAfterSelection={(selectedItem, index) => selectedItem}
-          buttonTextStyle={globalStyles.labelPrimary}
-        />
-      </View>
-      <Text style={[globalStyles.bodyLargePrimaryGray, styles.bodyContainer]}>
-        1월 15일
-      </Text>
-      {/*FlatList로 관심 팝업 모델객체 만들어서 후에 렌더링*/}
-      <DividerLine height={3} />
-      <InterestPopUpCard
-        Svg={InterestSampleSvg} // 필요한 경우 다른 SVG로 대체
-        title="팝업 스토어 이름1"
-        date="2024.01.01-2024.02.02"
-        status={'운영 중'}
-      />
-      <DividerLine height={3} />
-      <InterestPopUpCard
-        Svg={InterestSampleSvg} // 필요한 경우 다른 SVG로 대체
-        title="팝업 스토어 이름1"
-        date="2024.01.01-2024.02.02"
-        status={'운영 중'}
-      />
-      <DividerLine height={3} />
-      <InterestPopUpCard
-        Svg={InterestSampleSvg} // 필요한 경우 다른 SVG로 대체
-        title="팝업 스토어 이름1"
-        date="2024.01.01-2024.02.02"
-        status={'운영 중'}
-      />
-      <DividerLine height={3} />
       <View />
     </SafeAreaView>
   );
@@ -102,11 +312,30 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: 'white',
   },
+  bottomSheetContent: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  titleContainer: {
+    width: '100%', // Ensure the container takes the full width
+    paddingHorizontal: 16, // Add some padding
+  },
+  bottomSheetBackground: {
+    backgroundColor: 'white',
+    alignItems: 'flex-start',
+  },
+  bottomSheetHandle: {},
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 0.8,
+  },
   labelSmallBlue: {
     fontFamily: 'Pretandard-Regular',
     fontSize: 12,
     fontWeight: '400',
     color: primaryColors.blue,
+    marginRight: 10,
   },
   headerContainer: {
     flexDirection: 'row',

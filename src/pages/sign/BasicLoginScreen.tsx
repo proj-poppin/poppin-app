eb2d4b467691473ea118c6ba81634f16d5ab8acb
puretension <rlrlfhtm5@gmail.com>
2024-04-01 02:35:50 +0900

    Feat: 이메일로 로그인시 화면 생성

---
+++ src/pages/sign/BasicLoginScreen.tsx
@@ -0,0 +1,92 @@
+import DismissKeyboardView from '../../components/DismissKeyboardView.tsx';
+import MainTitle from '../../components/organisms/header/MainTitle.tsx';
+import LabelText20B from '../../components/atoms/label/LabelText20B.tsx';
+import CommonTextFormField from '../../components/molecules/form_field/CommonTextFormField.tsx';
+import React from 'react';
+import CompleteButton from '../../components/CompleteButton.tsx';
+import useBasicLogin from '../../hooks/useBasicLogin.tsx';
+import {StyleSheet, View} from 'react-native';
+import PressableGreyTextWord from '../../components/molecules/pressable_text/PressableGreyTextWord.tsx';
+
+function BasicLoginScreen({navigation}) {
+  const {
+    email,
+    password,
+    emailError,
+    passwordError,
+    isLoginButtonEnabled,
+    handleChangeEmail,
+    handleChangePassword,
+    handleLogin,
+  } = useBasicLogin();
+
+  console.log(email, password, emailError, passwordError, isLoginButtonEnabled);
+
+  const navigateToSignUpEmail = () => {
+    navigation.navigate('SignUpEmail');
+  };
+
+  const navigateToPasswordReset = () => {
+    navigation.navigate('PasswordReset');
+  };
+
+  return (
+    <DismissKeyboardView>
+      <View style={styles.container}>
+        <MainTitle
+          text1="내 취향을 기반으로"
+          text2="팝업 스토어를 관리하고 저장해요"
+          isPoppinLogo={true}
+        />
+        <LabelText20B text={'아이디'} />
+        <CommonTextFormField
+          onChangeText={handleChangeEmail}
+          onEndEditing={() => {}}
+          placeholder="이메일 주소를 입력해주세요."
+          keyboardType="email-address"
+          errorText={emailError}
+        />
+        <LabelText20B text={'비밀번호'} />
+        <CommonTextFormField
+          onChangeText={handleChangePassword}
+          placeholder="영문,숫자,특수문자 포함 8자 이상"
+          errorText={passwordError}
+        />
+        <CompleteButton
+          onPress={handleLogin}
+          title={'로그인'}
+          disabled={!isLoginButtonEnabled}
+        />
+        <View style={styles.rowContainer}>
+          <PressableGreyTextWord
+            onPressed={navigateToPasswordReset}
+            text="비밀번호 재설정"
+          />
+          <PressableGreyTextWord
+            onPressed={navigateToPasswordReset}
+            text=" | "
+          />
+          <PressableGreyTextWord
+            onPressed={navigateToSignUpEmail}
+            text="회원 가입"
+          />
+        </View>
+      </View>
+    </DismissKeyboardView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'white',
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+});
+
+export default BasicLoginScreen;

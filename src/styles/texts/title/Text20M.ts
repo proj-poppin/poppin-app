334fb5f3b42e8e69433e3caca458a15ca6930987
puretension <rlrlfhtm5@gmail.com>
2024-04-01 02:37:28 +0900

    Design: 텍스트 스타일 추가

---
+++ src/styles/texts/title/Text20M.ts
@@ -0,0 +1,11 @@
+// Text20M.ts
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  text: {
+    fontSize: 20,
+    fontWeight: '500',
+    fontFamily: 'Pretendard',
+    color: 'black',
+  },
+});

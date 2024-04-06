5bf0d282674ba3d3742393722bedc0076c830dc9
puretension <rlrlfhtm5@gmail.com>
2024-04-07 05:46:55 +0900

    Feat: PopUpList Response Dto

--- src/types/PopUpListData.ts
+++ src/types/PopUpListData.ts
@@ -4,3 +4,17 @@ export type GetPopUpListResponse = {
   name: string;
   introduce: string;
 };
+
+export type GetTastePopUpListResponse = {
+  taste: string; // 여기서는 taste가 문자열로 되어있습니다.
+  popupSummaryDtos: GetPopUpListResponse[]; // 이전에 정의한 GetPopUpListResponse 타입의 배열
+};
+
+export type GetInterestPopUpListResponse = {
+  id: number;
+  image_url: string;
+  status: string;
+  name: string;
+  open_date: string;
+  close_date: string;
+};

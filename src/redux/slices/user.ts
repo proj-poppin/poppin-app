97ecb9044b3189972b4eeb5b67ee350bb0c096fc
puretension <rlrlfhtm5@gmail.com>
2024-04-06 20:15:12 +0900

    Feat: userSlice 속성, getter, setter 추가

--- src/redux/slices/user.ts
+++ src/redux/slices/user.ts
@@ -1,23 +1,52 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
-  nickname: '',
+  birthDate: '',
   email: '',
+  nickname: '',
+  provider: '',
+  userImageUrl: '',
+  password: '',
+  passwordConfirm: '',
+  agreedToPrivacyPolicy: true,
+  agreedToServiceTerms: true,
   accessToken: '',
   phoneToken: '',
   money: 0,
   isSocialLogin: false,
+  isFinishedPreferenceSetting: true,
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUser(state, action) {
+    getUser(state, action) {
+      state.birthDate = action.payload.birthDate;
       state.email = action.payload.email;
       state.nickname = action.payload.nickname;
-      state.accessToken = action.payload.accessToken;
+      state.provider = action.payload.provider;
+      state.userImageUrl = action.payload.userImageUrl;
     },
+    setSignUpEmailScreen(state, action) {
+      state.email = action.payload.email;
+      state.password = action.payload.password;
+      state.passwordConfirm = action.payload.passwordConfirm;
+      state.agreedToPrivacyPolicy = action.payload.agreedToPrivacyPolicy;
+      state.agreedToServiceTerms = action.payload.agreedToServiceTerms;
+    },
+    setSignUpNickNameScreen(state, action) {
+      state.nickname = action.payload.nickname;
+      state.birthDate = action.payload.birthDate;
+    },
+    setAgreedToPrivacyPolicy(state, action) {
+      state.agreedToPrivacyPolicy = action.payload;
+      state.agreedToServiceTerms = action.payload;
+    },
+    setIsFinishedPreferenceProcess(state, action) {
+      state.isFinishedPreferenceSetting = action.payload;
+    },
+
     setAccessToken(state, action) {
       state.accessToken = action.payload;
     },
@@ -27,7 +56,16 @@ const userSlice = createSlice({
     setPhoneToken(state, action) {
       state.phoneToken = action.payload;
     },
+    resetUser(state) {
+      state.nickname = '';
+      state.email = '';
+      state.accessToken = '';
+      state.phoneToken = '';
+      state.money = 0;
+      state.isSocialLogin = false;
+    },
   },
 });
 
+export const {resetUser} = userSlice.actions;
 export default userSlice;

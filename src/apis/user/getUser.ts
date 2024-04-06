192be7fc716cf47b437638e25535d2ca04614294
puretension <rlrlfhtm5@gmail.com>
2024-04-06 20:42:07 +0900

    Feat: 내정보 불러오기 api

---
+++ src/apis/user/getUser.ts
@@ -0,0 +1,39 @@
+import apiInstance from '../axios.ts';
+
+export interface GetUserData {
+  userImageUrl: string;
+  email: string;
+  nickname: string;
+  birthDate: string;
+  provider: string;
+}
+const getUser = async (): Promise<CommonResponse<GetUserData>> => {
+  try {
+    const response = await apiInstance.get('/api/v1/user');
+    console.log('Sign up response:', response.data);
+
+    if (response.data.success) {
+      console.log('Sign up successful', response.data);
+      return response.data;
+    } else {
+      // API 응답이 success=false인 경우
+      return {
+        success: false,
+        data: null,
+        error: response.data.error,
+      };
+    }
+  } catch (error) {
+    console.error('Error fetching hot list:', error);
+    // 네트워크 에러 또는 기타 예외 처리
+    return {
+      success: false,
+      data: null,
+      error: {
+        code: 'NetworkError',
+        message: 'Network error occurred',
+      },
+    };
+  }
+};
+export default getUser;

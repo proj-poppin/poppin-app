aa964a8aff47f2a87778bd644d9ce457e9f6e7d8
puretension <rlrlfhtm5@gmail.com>
2024-04-06 20:40:20 +0900

    Feat: 자체회원가입시 이메일 인증

---
+++ src/apis/auth/emailVerification.ts
@@ -0,0 +1,46 @@
+import Config from 'react-native-config';
+import axios from 'axios';
+
+interface AuthCodeResponseData {
+  authCode: string;
+}
+
+// 이메일 인증 코드 요청 함수
+const emailVerification = async (
+  email: string,
+): Promise<CommonResponse<AuthCodeResponseData>> => {
+  try {
+    const response = await axios.post<CommonResponse<AuthCodeResponseData>>(
+      `${Config.API_URL}/api/v1/auth/email/verification`,
+      {email},
+    );
+
+    console.log('Server response:', response.data);
+
+    if (response.data.success && response.data.data) {
+      console.log(
+        `Email verification success, AuthCode: ${response.data.data.authCode}`,
+      );
+      return {
+        success: true,
+        data: {authCode: response.data.data.authCode},
+        error: null,
+      };
+    } else {
+      return {
+        success: false,
+        data: null,
+        error: response.data.error,
+      };
+    }
+  } catch (error) {
+    console.error('Email verification error:', error);
+    return {
+      success: false,
+      data: {authCode: ''},
+      error: {code: 'Network', message: 'Network error'},
+    };
+  }
+};
+
+export default emailVerification;

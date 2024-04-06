1dcab02368f440367cca10c82c61e53abc6d7295
puretension <rlrlfhtm5@gmail.com>
2024-04-06 20:46:21 +0900

    Feat: reducer 추가

--- src/redux/stores/reducer.ts
+++ src/redux/stores/reducer.ts
@@ -1,14 +1,15 @@
 import {combineReducers} from '@reduxjs/toolkit';
 import userSlice from '../slices/user.ts';
-import loadingReducer from '../slices/loading.ts';
 import loadingSlice from '../slices/loading.ts';
 import bottomSheetSlice from '../slices/bottomSheetSlice.ts';
+import preferenceSlice from '../slices/preferenceSlice.ts';
 
 // 모든 상태를 결합
 const rootReducer = combineReducers({
   user: userSlice.reducer,
-  loading: loadingSlice.reducer, // loading 상태 추가
-  bottomSheet: bottomSheetSlice.reducer, // 추가
+  loading: loadingSlice.reducer,
+  bottomSheet: bottomSheetSlice.reducer,
+  preference: preferenceSlice.reducer,
 });
 
 export default rootReducer;

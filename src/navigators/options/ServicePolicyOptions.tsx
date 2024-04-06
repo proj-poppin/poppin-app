19e64e97e4793d8c7119fa6f001f2b3d156db3b5
puretension <rlrlfhtm5@gmail.com>
2024-04-06 20:37:12 +0900

    Feat: 정책화면들 구현

---
+++ src/navigators/options/ServicePolicyOptions.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Pressable} from 'react-native';
+import BackSvg from '../../assets/icons/goBack.svg';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {AuthNavigatorParamList} from '../../types/AuthNavigatorParamList.ts';
+
+type ServicePolicyScreenNavigationProp = NativeStackNavigationProp<
+  AuthNavigatorParamList,
+  'ServicePolicy'
+>;
+
+// Props 타입 정의
+// HeaderOptions 컴포넌트
+const ServicePolicyOptions = ({
+  navigation,
+}: {
+  navigation: ServicePolicyScreenNavigationProp;
+}) => ({
+  headerLeft: () => (
+    <Pressable onPress={() => navigation.goBack()} style={{padding: 10}}>
+      <BackSvg />
+    </Pressable>
+  ),
+  headerTitle: '서비스 이용약관',
+});
+
+export default ServicePolicyOptions;
---
+++ src/pages/sign/ServicePolicyScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+function ServicePolicyScreen() {
+  return (
+    <View>
+      <Text>Service Policy Screen</Text>
+    </View>
+  );
+}
+
+export default ServicePolicyScreen;

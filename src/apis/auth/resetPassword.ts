5445ef6ac9c68cfe4750bb5ba9ba7aab0bbc7b03
puretension <rlrlfhtm5@gmail.com>
2024-04-06 20:42:47 +0900

    Feat: 비밀번호 재설정 api

---
+++ src/apis/auth/resetPassword.ts
@@ -0,0 +1,32 @@
+import axios from '../axios.ts';
+import Config from 'react-native-config';
+
+// 회원가입을 시도하는 함수
+const resetPassword = async (password: string, passwordConfirm: string) => {
+  try {
+    const response = await axios.put(
+      `${Config.API_URL}/api/v1/auth/reset-password`,
+      {
+        password,
+        passwordConfirm,
+      },
+    );
+    if (response.data.success) {
+      console.log('Sign up successful', response.data);
+      return response.data;
+    } else {
+      return {
+        success: false,
+        error: response.data.error,
+      };
+    }
+  } catch (error) {
+    console.error('Email verification error:', error);
+    return {
+      success: false,
+      error: {code: 'Network', message: 'Network error'},
+    };
+  }
+};
+
+export default resetPassword;

fddd44c11e51c7bffaf4561ddf3938cd568060c2
sohee634 <soan32@hanmail.net>
2024-05-06 09:41:31 +0900

    Feact: 알림세팅 ui 구현

--- src/components/alarm/SettingSwitch.tsx
+++ src/components/alarm/SettingSwitch.tsx
@@ -1,28 +1,60 @@
 import {useState} from 'react';
-import {Switch, Text} from 'react-native';
+import {StyleSheet, Switch, Text, View} from 'react-native';
+import globalColors from '../../styles/color/globalColors';
 
 type Props = {
   label: string;
+  desc?: string;
+  name: string;
+  onChange: (name: string, value: boolean) => void;
 };
 
-function SettingSwitch({label}: Props) {
+function SettingSwitch({label, desc, name, onChange}: Props) {
   const [isAlarmAll, setIsAlarmAll] = useState(false);
 
   const toggleSwitch = () => {
     setIsAlarmAll(previousState => !previousState);
+    onChange(name, isAlarmAll);
   };
   return (
-    <>
-      <Text>{label}</Text>
+    <View style={styles.container}>
+      <View style={styles.textWrapper}>
+        <Text style={styles.title}>{label}</Text>
+        {desc && <Text style={styles.desc}>{desc}</Text>}
+      </View>
+
       <Switch
-        trackColor={{false: 'red', true: '#s81b0ff'}}
-        thumbColor={isAlarmAll ? '#f5dd4b' : '#f4f3f4'}
-        ios_backgroundColor="#3e3e3e"
+        trackColor={{false: globalColors.stroke2, true: globalColors.blue}}
+        thumbColor={isAlarmAll ? 'white' : 'white'}
+        ios_backgroundColor="#DDDDDD"
         onValueChange={toggleSwitch}
         value={isAlarmAll}
       />
-    </>
+    </View>
   );
 }
 
 export default SettingSwitch;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textWrapper: {
+    textAlign: 'center',
+    display: 'flex',
+    gap: 5,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 18,
+  },
+  desc: {
+    fontSize: 13,
+    color: globalColors.stroke2,
+  },
+});
--- src/pages/Alarm/AlaramSettingScreen.tsx
+++ src/pages/Alarm/AlaramSettingScreen.tsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import {Text, Switch, View, StyleSheet} from 'react-native';
 import SettingSwitch from '../../components/alarm/SettingSwitch';
+import globalColors from '../../styles/color/globalColors';
 
 function AlaramSettingScreen() {
   const [isAlarmAll, setIsAlarmAll] = useState(false);
@@ -9,23 +10,52 @@ function AlaramSettingScreen() {
     setIsAlarmAll(previousState => !previousState);
   };
 
+  const onChange = (name: string, value: boolean) => {};
+
   return (
     <View style={styles.container}>
-      <View style={styles.alarmAll}>
-        <SettingSwitch label="알림 전체 끄기" />
+      <View style={styles.content}>
+        <View>
+          <Text style={styles.label}>기본 알림</Text>
+        </View>
+        <SettingSwitch label="푸시 알림" name="push" onChange={onChange} />
+        <SettingSwitch
+          label="야간 푸시 알림(21시~08시)"
+          name="nightPush"
+          onChange={onChange}
+        />
       </View>
-      <View>
-        <Text>기본 알림</Text>
+      <View style={styles.diver} />
+
+      <View style={styles.content}>
+        <View>
+          <Text style={styles.label}>활동 알림</Text>
+        </View>
+        <SettingSwitch
+          label="도움이 된 후기"
+          desc="내가 작성한 후기에 추천이 달리면 알려드려요"
+          name="feedback"
+          onChange={onChange}
+        />
+        <SettingSwitch
+          label="관심 팝업 오픈"
+          desc="관심 등록한 팝업이 오픈되면 알려드려요"
+          name="favorite"
+          onChange={onChange}
+        />
+        <SettingSwitch
+          label="관심 팝업 마감 D-1"
+          desc="관심 등록한 팝업 D-1알려드려요"
+          name="favoriteDeadline"
+          onChange={onChange}
+        />
+        <SettingSwitch
+          label="관심 팝업 정보 변경"
+          desc="관심 등록한 팝업 정보가 업데이트되면 알려드려요"
+          name="favoriteEdite"
+          onChange={onChange}
+        />
       </View>
-      <SettingSwitch label="푸시 알림" />
-      <SettingSwitch label="야간 푸시 알림(21시~08시)" />
-      <View>
-        <Text>기본 알림</Text>
-      </View>
-      <SettingSwitch label="도움이 된 후기" />
-      <SettingSwitch label="관심 팝업 오픈" />
-      <SettingSwitch label="관심 팝업 마감 D-1" />
-      <SettingSwitch label="관심 팝업 정보 변경" />
     </View>
   );
 }
@@ -35,16 +65,21 @@ export default AlaramSettingScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 15,
+    paddingTop: 15,
     gap: 20,
   },
-  alarmAll: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 7,
-    backgroundColor: 'skyblue',
+  content: {
     padding: 15,
-    borderRadius: 100,
+    display: 'flex',
+    gap: 15,
+  },
+  diver: {
+    width: '100%',
+    height: 10,
+    backgroundColor: globalColors.warmGray,
+  },
+
+  label: {
+    color: globalColors.stroke2,
   },
 });

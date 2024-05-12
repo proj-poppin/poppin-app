f7e62dea7af81d93460755a190ba3fe76f0df135
puretension <rlrlfhtm5@gmail.com>
2024-05-12 09:00:54 +0900

    Fix: 바텀시트 에러 수정

--- src/components/atoms/button/FilterSettingButton.tsx
+++ src/components/atoms/button/FilterSettingButton.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import {TouchableOpacity, Text, StyleSheet, View} from 'react-native';
 import FilterSvg from '../../../assets/icons/filter.svg';
-import globalColors from '../../../styles/color/globalColors.ts';
-const FilterSettingsButton = ({onPress}) => {
+import globalColors from '../../../styles/color/globalColors';
+
+interface FilterSettingsButtonProps {
+  onPress: () => void;
+  isSetting: boolean;
+}
+
+const FilterSettingsButton: React.FC<FilterSettingsButtonProps> = ({
+  onPress,
+  isSetting,
+}) => {
   return (
     <TouchableOpacity style={styles.button} onPress={onPress}>
       <View style={styles.iconContainer}>
         <FilterSvg />
       </View>
-      <Text style={styles.text}>필터 설정</Text>
+      {isSetting ? (
+        <Text style={styles.text}>필터 적용</Text>
+      ) : (
+        <Text style={styles.text}>필터 설정</Text>
+      )}
     </TouchableOpacity>
   );
 };
@@ -24,19 +37,18 @@ const styles = StyleSheet.create({
     height: 35,
     alignItems: 'center',
     justifyContent: 'center',
-    shadowOffset: {
-      width: 2,
-      height: 2,
-    },
+    shadowOffset: {width: 2, height: 2},
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
   },
   iconContainer: {
     flexDirection: 'row',
-    paddingRight: 10, // space between icons and text
+    paddingRight: 10, // space between icon and text
   },
-  text: {},
+  text: {
+    color: globalColors.text, // Assuming there's a text color defined in globalColors
+  },
 });
 
 export default FilterSettingsButton;

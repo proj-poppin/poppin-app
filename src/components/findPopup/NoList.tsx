29bc0f0203424229f7c88c8f81c281eecdb90f3d
sohee-an <soan32@hanmail.net>
2024-05-18 12:54:33 +0900

    Styles : 스타일 수정

--- src/components/findPopup/NoList.tsx
+++ src/components/findPopup/NoList.tsx
@@ -1,34 +1,18 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import FindPopupNoList from '../../assets/images/findPopupNoList.svg';
 import globalColors from '../../styles/color/globalColors';
 import NoListText from '../../assets/images/findPopupText.svg';
 
 function NoList() {
   return (
-    <View
-      style={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        paddingTop: 50,
-      }}>
+    <View style={styles.container}>
       <Text style={{fontSize: 20, marginBottom: 20}}>
         조건에 해당하는 팝업 이벤트가 없어요😥
       </Text>
       <FindPopupNoList width="300" height={200} />
       <NoListText width={250} height={80} />
-      <View
-        style={{
-          width: '90%',
-          height: 80,
-          backgroundColor: globalColors.blue,
-          borderRadius: 80,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
+      <View style={styles.textWrapper}>
         <Text style={{fontSize: 25, color: 'white'}}>제보하러 가기</Text>
       </View>
     </View>
@@ -36,3 +20,22 @@ function NoList() {
 }
 
 export default NoList;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: 50,
+  },
+  textWrapper: {
+    width: '90%',
+    height: 80,
+    backgroundColor: globalColors.blue,
+    borderRadius: 80,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});

00d56d9fcd8f91ff45dada7092876dc9e0882da2
puretension <rlrlfhtm5@gmail.com>
2024-05-21 19:01:58 +0900

    Refactor: 로그인 필요 상자 파라미터 추가 #41

--- src/components/NotLogginBox.tsx
+++ src/components/NotLogginBox.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import {View, Text, StyleSheet, Pressable} from 'react-native';
 import globalColors from '../styles/color/globalColors.ts';
+import LoadingLoopSvg from '../assets/likes/loadingLoop.svg';
+
+type NotLogginBoxProps = {
+  text1: string;
+  text2: string;
+  buttonText: string;
+  onPress: () => void;
+  isNeedBox?: boolean;
+  isNeedSvg?: boolean;
+};
 
 const NotLogginBox = ({
   text1,
@@ -8,9 +18,15 @@ const NotLogginBox = ({
   buttonText,
   onPress,
   isNeedBox = true,
-}) => {
+  isNeedSvg = true,
+}: NotLogginBoxProps) => {
   return (
-    <View style={isNeedBox ? styles.shadowedBox : styles.boxWithoutShadow}>
+    <View
+      style={[
+        styles.container,
+        isNeedBox ? styles.shadowedBox : styles.boxWithoutShadow,
+      ]}>
+      {isNeedSvg && <LoadingLoopSvg style={{alignItems: 'center'}} />}
       <View style={styles.contentContainer}>
         <Text style={styles.text}>
           {text1} {'\n'}
@@ -25,6 +41,11 @@ const NotLogginBox = ({
 };
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
   shadowedBox: {
     backgroundColor: globalColors.white,
     borderRadius: 10,

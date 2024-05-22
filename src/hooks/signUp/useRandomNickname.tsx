9567f5f2bb9859a3d2d9b152e66bb212fc852efa
puretension <rlrlfhtm5@gmail.com>
2024-05-23 05:53:59 +0900

    Feat: 랜덤 닉네임 API 생성 #11

---
+++ src/hooks/signUp/useRandomNickname.tsx
@@ -0,0 +1,31 @@
+import {useState, useEffect} from 'react';
+import randomNickName from '../../apis/auth/randomNickName.ts';
+
+const useRandomNickname = () => {
+  const [nickname, setNickname] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchNickname = async () => {
+      try {
+        const response = await randomNickName();
+        if (response.success) {
+          setNickname(response.data.randomNickname);
+        } else {
+          setError(response.error);
+        }
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNickname();
+  }, []);
+
+  return {nickname, loading, error};
+};
+
+export default useRandomNickname;

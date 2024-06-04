1e910604d2912e81bbf0c169dc0a375a598c2b26
puretension <rlrlfhtm5@gmail.com>
2024-06-04 22:14:51 +0900

    Fix: svg import 경로 수정 및 xdate 패키지 추가

--- src/pages/likes/calendar/HeaderTitle.tsx
+++ src/pages/likes/calendar/HeaderTitle.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable */
-import { Text, TouchableOpacity, View } from "react-native";
+import {Text, TouchableOpacity, View} from 'react-native';
 import XDate from 'xdate';
-import React from "react";
-import VButton from "/Users/gwagmin-u/Documents/0_workspace/react-native/poppin-app/src/assets/likes/VButton.svg"
-import { DateData } from "react-native-calendars";
-import RNDateTimePicker from "@react-native-community/datetimepicker";
-import DateTimePicker from "react-native-modal-datetime-picker";
+import React from 'react';
+import VButton from '../../../assets/likes/VButton.svg';
+import {DateData} from 'react-native-calendars';
+import RNDateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePicker from 'react-native-modal-datetime-picker';
 
 const stringToDateData = (dateString: string): DateData => {
   const date = new XDate(dateString);
@@ -15,7 +15,7 @@ const stringToDateData = (dateString: string): DateData => {
     month: date.getMonth() + 1, // Months are zero-based in XDate
     day: date.getDate(),
     timestamp: date.getTime(),
-    dateString: date.toString('yyyy-MM-dd')
+    dateString: date.toString('yyyy-MM-dd'),
   };
 };
 
@@ -24,21 +24,30 @@ interface HeaderTitleProps {
   onClickHeaderTitle: (dateData: DateData) => void;
 }
 
-const HeaderTitle: React.FC<HeaderTitleProps> = ({selDate, onClickHeaderTitle}) => {
+const HeaderTitle: React.FC<HeaderTitleProps> = ({
+  selDate,
+  onClickHeaderTitle,
+}) => {
   const dateData: DateData = stringToDateData(selDate);
   return (
     <TouchableOpacity
       onPress={() => {
         onClickHeaderTitle(dateData);
-      }}
-    >
-      <View style={{flexDirection:"row", justifyContent:"center", alignItems:"center", marginBottom:20}}>
-        <Text style={{fontWeight:"600", fontSize:20, marginRight:8}}>{dateData?.year}.{dateData?.month}</Text>
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginBottom: 20,
+        }}>
+        <Text style={{fontWeight: '600', fontSize: 20, marginRight: 8}}>
+          {dateData?.year}.{dateData?.month}
+        </Text>
         <VButton />
       </View>
     </TouchableOpacity>
-
   );
-}
+};
 
 export default HeaderTitle;

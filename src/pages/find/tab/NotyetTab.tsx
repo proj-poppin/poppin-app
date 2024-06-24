f879f07e70f9c1911ba55a5bfb19fffd316b9ef9
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-06-25 02:30:05 +0900

    Fix: 페이지네이션 에러 수정 #73

--- src/pages/find/tab/NotyetTab.tsx
+++ src/pages/find/tab/NotyetTab.tsx
@@ -1,10 +1,10 @@
+import {ActivityIndicator, ScrollView, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
-import {ScrollView, ActivityIndicator, View} from 'react-native';
 import DividerLine from '../../../components/DividerLine.tsx';
 import FindCard from '../../../components/findPopup/FindCard.tsx';
 import useGetFindPopupList from '../../../hooks/findPopUp/useGetFindPopupList.tsx';
 import NotList from '../../../components/findPopup/NotList.tsx';
-import globalColors from '../../../styles/color/globalColors.ts'; // Ensure you import the right path
+import globalColors from '../../../styles/color/globalColors.ts';
 
 function NotyetTab({type, selectedOrder, availableTags, searchKeyword}: any) {
   const [page, setPage] = useState(0);
@@ -29,65 +29,45 @@ function NotyetTab({type, selectedOrder, availableTags, searchKeyword}: any) {
   const handleScroll = (event: any) => {
     const {layoutMeasurement, contentOffset, contentSize} = event.nativeEvent;
     const isEndReached =
-      layoutMeasurement.height + contentOffset.y >= contentSize.height - 20;
+      layoutMeasurement.height + contentOffset.y >= contentSize.height;
     if (isEndReached && !loadingMore) {
       setLoadingMore(true);
-      setPage(prevPage => prevPage + 1);
-      setTriggerFetch(true);
+
+      setPage(page + 1);
     }
   };
-
   useEffect(() => {
     setPage(0);
     setTriggerFetch(true);
   }, [selectedOrder, availableTags, searchKeyword]);
 
   useEffect(() => {
-    if (!findPopupListLoading) {
-      setLoadingMore(false);
-      if (triggerFetch) {
-        setTriggerFetch(false);
-      }
+    if (triggerFetch) {
+      setTriggerFetch(false);
     }
-  }, [findPopupListLoading, triggerFetch]);
-
-  if (findPopupListLoading && page === 0) {
+  }, [triggerFetch]);
+  if (findPopupListData.length === 0 && !findPopupListLoading) {
+    return <NotList />;
+  } else if (findPopupListLoading && page === 0) {
     return (
-      <View style={styles.centered}>
+      <View>
         <ActivityIndicator size="large" color={globalColors.purple} />
       </View>
     );
   }
-
-  if (findPopupListData.length === 0 && !findPopupListLoading) {
-    return <NotList />;
-  }
-
   return (
     <ScrollView onScroll={handleScroll} style={{marginBottom: 100}}>
       <DividerLine height={1} />
-      {findPopupListData.map((item: any) => (
-        <FindCard type={type} key={item.id} item={item} />
-      ))}
-      {loadingMore && (
-        <View style={styles.loadingMore}>
-          <ActivityIndicator size="small" color={globalColors.blue} />
-        </View>
+      {findPopupListData && findPopupListData.length > 0 ? (
+        findPopupListData.map((item: any) => {
+          return <FindCard type={type} key={item.id} item={item} />;
+        })
+      ) : (
+        <NotList />
       )}
       <DividerLine height={1} />
     </ScrollView>
   );
 }
-
-const styles = {
-  centered: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  loadingMore: {
-    paddingVertical: 20,
-  },
-};
 
 export default NotyetTab;
--- src/pages/find/tab/OperatonTab.tsx
+++ src/pages/find/tab/OperatonTab.tsx
@@ -1,10 +1,111 @@
-import {ScrollView, ActivityIndicator, View} from 'react-native';
+// import {ScrollView, ActivityIndicator, View} from 'react-native';
+// import React, {useEffect, useState} from 'react';
+// import DividerLine from '../../../components/DividerLine.tsx';
+// import FindCard from '../../../components/findPopup/FindCard.tsx';
+// import useGetFindPopupList from '../../../hooks/findPopUp/useGetFindPopupList.tsx';
+// import NotList from '../../../components/findPopup/NotList.tsx';
+// import globalColors from '../../../styles/color/globalColors.ts'; // Ensure you import the right path
+//
+// function OperationTab({
+//   type,
+//   selectedOrder,
+//   availableTags,
+//   searchKeyword,
+// }: any) {
+//   const [page, setPage] = useState(0);
+//   const [size, setSize] = useState(5);
+//   const [loadingMore, setLoadingMore] = useState(false);
+//   const [triggerFetch, setTriggerFetch] = useState(false);
+//
+//   const {
+//     data: findPopupListData,
+//     loading: findPopupListLoading,
+//     error: findPopupListError,
+//   } = useGetFindPopupList(
+//     page,
+//     size,
+//     'OPERATING',
+//     selectedOrder,
+//     availableTags,
+//     searchKeyword,
+//     triggerFetch,
+//   );
+//
+//   const handleScroll = (event: any) => {
+//     const {layoutMeasurement, contentOffset, contentSize} = event.nativeEvent;
+//     const isEndReached =
+//       layoutMeasurement.height + contentOffset.y >= contentSize.height - 20;
+//     if (isEndReached && !loadingMore) {
+//       setLoadingMore(true);
+//       setPage(prev => prev + 1);
+//       setTriggerFetch(true);
+//     }
+//   };
+//
+//   useEffect(() => {
+//     setPage(0);
+//     setTriggerFetch(true);
+//   }, [selectedOrder, availableTags, searchKeyword]);
+//
+//   useEffect(() => {
+//     if (!findPopupListLoading) {
+//       setLoadingMore(false);
+//       if (triggerFetch) {
+//         setTriggerFetch(false);
+//       }
+//     }
+//   }, [findPopupListLoading, triggerFetch]);
+//
+//   if (findPopupListLoading && page === 0) {
+//     return (
+//       <View style={styles.centered}>
+//         <ActivityIndicator size="large" color={globalColors.purple} />
+//       </View>
+//     );
+//   }
+//
+//   if (page === 1 && findPopupListData.length === 0 && !findPopupListLoading) {
+//     return <NotList />;
+//   }
+//
+//   return (
+//     <ScrollView
+//       scrollEventThrottle={16}
+//       onScroll={handleScroll}
+//       style={{marginBottom: 100}}>
+//       <DividerLine height={1} />
+//       {findPopupListData.map((item: any, index: number) => (
+//         <FindCard type={type} key={`${item.id}-${index}`} item={item} />
+//       ))}
+//       {loadingMore && (
+//         <View style={styles.loadingMore}>
+//           <ActivityIndicator size="small" color={globalColors.blue} />
+//         </View>
+//       )}
+//       <DividerLine height={1} />
+//     </ScrollView>
+//   );
+// }
+//
+// const styles = {
+//   centered: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   loadingMore: {
+//     paddingVertical: 20,
+//   },
+// };
+//
+// export default OperationTab;
+import {ActivityIndicator, ScrollView, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import DividerLine from '../../../components/DividerLine.tsx';
 import FindCard from '../../../components/findPopup/FindCard.tsx';
 import useGetFindPopupList from '../../../hooks/findPopUp/useGetFindPopupList.tsx';
 import NotList from '../../../components/findPopup/NotList.tsx';
-import globalColors from '../../../styles/color/globalColors.ts'; // Ensure you import the right path
+import globalColors from '../../../styles/color/globalColors.ts';
 
 function OperationTab({
   type,
@@ -34,11 +135,10 @@ function OperationTab({
   const handleScroll = (event: any) => {
     const {layoutMeasurement, contentOffset, contentSize} = event.nativeEvent;
     const isEndReached =
-      layoutMeasurement.height + contentOffset.y >= contentSize.height - 20;
+      layoutMeasurement.height + contentOffset.y >= contentSize.height;
     if (isEndReached && !loadingMore) {
       setLoadingMore(true);
       setPage(prev => prev + 1);
-      setTriggerFetch(true);
     }
   };
 
@@ -48,24 +148,19 @@ function OperationTab({
   }, [selectedOrder, availableTags, searchKeyword]);
 
   useEffect(() => {
-    if (!findPopupListLoading) {
-      setLoadingMore(false);
-      if (triggerFetch) {
-        setTriggerFetch(false);
-      }
+    if (triggerFetch) {
+      setTriggerFetch(false);
     }
-  }, [findPopupListLoading, triggerFetch]);
-
-  if (findPopupListLoading && page === 0) {
-    return (
-      <View style={styles.centered}>
-        <ActivityIndicator size="large" color={globalColors.purple} />
-      </View>
-    );
-  }
+  }, [triggerFetch]);
 
   if (findPopupListData.length === 0 && !findPopupListLoading) {
     return <NotList />;
+  } else if (findPopupListLoading && page === 0) {
+    return (
+      <View>
+        <ActivityIndicator size="large" color={globalColors.purple} />
+      </View>
+    );
   }
 
   return (
@@ -74,28 +169,27 @@ function OperationTab({
       onScroll={handleScroll}
       style={{marginBottom: 100}}>
       <DividerLine height={1} />
-      {findPopupListData.map((item: any) => (
-        <FindCard type={type} key={item.id} item={item} />
-      ))}
-      {loadingMore && (
-        <View style={styles.loadingMore}>
-          <ActivityIndicator size="small" color={globalColors.blue} />
+      {findPopupListData && findPopupListData.length > 0 ? (
+        findPopupListData.map((item: any) => (
+          <FindCard type={type} key={item.id} item={item} />
+        ))
+      ) : (
+        <NotList />
+      )}
+      {findPopupListLoading && (
+        <View
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>데이터를 불러오고 있습니다.</Text>
         </View>
       )}
       <DividerLine height={1} />
     </ScrollView>
   );
 }
-
-const styles = {
-  centered: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  loadingMore: {
-    paddingVertical: 20,
-  },
-};
 
 export default OperationTab;

6263fce041fc9c500a197d1d4747c3ac6700608c
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-06-30 10:58:54 +0900

    Design: 토스트 UI 수정 #76

--- src/components/atoms/toast/ToastComponent.tsx
+++ src/components/atoms/toast/ToastComponent.tsx
@@ -1,7 +1,6 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {Text, Animated, StyleSheet, ViewStyle} from 'react-native';
-import Text14B from '../../../styles/texts/body_medium/Text14B.ts';
-import Text12B from '../../../styles/texts/label/Text12B.ts';
+import Text14B from '../../../styles/texts/body_medium/Text14B';
 
 interface ToastComponentProps {
   message: string;
@@ -9,53 +8,55 @@ interface ToastComponentProps {
   onClose: () => void;
   duration?: number;
   fadeDuration?: number;
+  bottom?: string | number; // Add bottom prop
 }
 
 const ToastComponent: React.FC<ToastComponentProps> = ({
   message,
-  height = 40,
+  height = 35,
   onClose,
-  duration = 3000,
+  duration = 1500,
   fadeDuration = 500,
+  bottom = '40%', // Default value for bottom
 }) => {
-  const [isToastVisible, setIsToastVisible] = useState(true);
   const fadeAnim = useRef(new Animated.Value(0)).current;
+
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsToastVisible(false);
-      onClose();
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: fadeDuration,
+        useNativeDriver: true,
+      }).start(() => {
+        onClose();
+      });
     }, duration);
 
-    return () => clearTimeout(timer);
-  }, [duration, onClose]);
-
-  useEffect(() => {
     Animated.timing(fadeAnim, {
-      toValue: isToastVisible ? 1 : 0,
+      toValue: 1,
       duration: fadeDuration,
       useNativeDriver: true,
     }).start();
-  }, [isToastVisible, fadeDuration, fadeAnim]);
+
+    return () => clearTimeout(timer);
+  }, [duration, fadeDuration, onClose, fadeAnim]);
 
   const styles = StyleSheet.create({
     container: {
       position: 'absolute',
+      bottom: typeof bottom === 'string' ? bottom : `${bottom}%`, // Apply bottom prop
+      alignSelf: 'center',
       alignItems: 'center',
       justifyContent: 'center',
       width: 368,
       height: height,
-      borderRadius: 14,
-      marginTop: 110,
-      marginLeft: 16,
-      marginRight: 16,
-      backgroundColor: 'rgba(0, 0, 0, 0.7)',
-      opacity: 1,
+      borderRadius: 20,
+      backgroundColor: 'rgba(0, 0, 0, 0.75)',
+      zIndex: 2,
     } as ViewStyle,
     toastText: {
       width: '100%',
       height: height,
-      marginTop: 10,
-      marginLeft: 10,
       lineHeight: 20,
       justifyContent: 'center',
       textAlign: 'center',
@@ -63,12 +64,8 @@ const ToastComponent: React.FC<ToastComponentProps> = ({
     },
   });
 
-  if (!isToastVisible) {
-    return null;
-  }
-
   return (
-    <Animated.View style={styles.container}>
+    <Animated.View style={[styles.container, {opacity: fadeAnim}]}>
       <Text style={[styles.toastText, Text14B.text, {color: 'white'}]}>
         {message}
       </Text>

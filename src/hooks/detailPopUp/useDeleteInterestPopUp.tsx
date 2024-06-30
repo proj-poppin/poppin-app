c096770c53ba826fcc2c3047475e806be506cff4
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-06-30 10:55:35 +0900

    Feat: 관심팝업 추가삭제시 Spinner, 상태 관리  #76

--- src/hooks/detailPopUp/useDeleteInterestPopUp.tsx
+++ src/hooks/detailPopUp/useDeleteInterestPopUp.tsx
@@ -1,11 +1,12 @@
 import {useState} from 'react';
 import getDeletePopUp from '../../apis/popup/deleteInterestPopUp.ts';
-import EncryptedStorage from "react-native-encrypted-storage";
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 interface DeleteInterestState {
   loading: boolean;
   error: Error | null;
   success: boolean | null;
+  message: string | null;
 }
 
 const useDeleteInterestPopUp = () => {
@@ -14,16 +15,27 @@ const useDeleteInterestPopUp = () => {
       loading: false,
       error: null,
       success: null,
+      message: null,
     });
 
   const deleteInterest = async (popupId: number) => {
-    setDeleteInterestState({loading: true, error: null, success: null});
+    setDeleteInterestState({
+      loading: true,
+      error: null,
+      success: null,
+      message: null,
+    });
 
     try {
       const fcm_token = (await EncryptedStorage.getItem('pushToken')) ?? '';
       const response = await getDeletePopUp({popupId, fcm_token});
       if (response.success) {
-        setDeleteInterestState({loading: false, error: null, success: true});
+        setDeleteInterestState({
+          loading: false,
+          error: null,
+          success: true,
+          message: '관심팝업에서 삭제되었어요!',
+        });
       } else {
         throw new Error(response.error?.message || 'Failed to delete interest');
       }
@@ -35,6 +47,7 @@ const useDeleteInterestPopUp = () => {
             ? error
             : new Error('An unexpected error occurred'),
         success: false,
+        message: null,
       });
     }
   };

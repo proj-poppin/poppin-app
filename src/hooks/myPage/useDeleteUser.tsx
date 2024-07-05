6034cf96d9ec13d2aeea8b885d52675b2ef8cc1e
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-07-06 03:33:12 +0900

    Feat: 회원 탈퇴 API 연동 #79

--- src/hooks/myPage/useDeleteUser.tsx
+++ src/hooks/myPage/useDeleteUser.tsx
@@ -1,50 +1,37 @@
-import {useState, useEffect} from 'react';
-import getHotList from '../../apis/popup/hotList.ts';
-import {GetPopUpListResponse} from '../../types/PopUpListData.ts';
+import {useState, useCallback} from 'react';
+import deleteUser, {GetUserInfoResponse} from '../../apis/myPage/deleteUser.ts';
 
-interface HotListState {
+interface DeleteUserState {
   loading: boolean;
   error: Error | null;
-  data: any | null;
+  data: GetUserInfoResponse | null;
 }
 
 const useDeleteUser = () => {
-  const [deleteUserState, setDeleteUserState] = useState<HotListState>({
+  const [deleteUserState, setDeleteUserState] = useState<DeleteUserState>({
     loading: false,
     error: null,
     data: null,
   });
 
-  useEffect(() => {
-    const deleteUser= async () => {
-      setDeleteUserState(prevState => ({...prevState, loading: true}));
-      try {
-        const response = await getHotList();
-        if (response.success) {
-          setDeleteUserState({loading: false, error: null, data: response.data}); 
-        } else {
-          setDeleteUserState({
-            loading: false,
-            error: new Error(response.error?.message || 'Unknown error'),
-            data: null,
-          });
-        }
-      } catch (error: any) {
-        setDeleteUserState({
-          loading: false,
-          error:
-            error instanceof Error
-              ? error
-              : new Error('An unexpected error occurred'),
-          data: null,
-        });
-      }
-    };
-
-    deleteUser();
+  const handleDeleteUser = useCallback(async () => {
+    setDeleteUserState({loading: true, error: null, data: null});
+    try {
+      const response = await deleteUser();
+      setDeleteUserState({loading: false, error: null, data: response});
+    } catch (error: any) {
+      setDeleteUserState({
+        loading: false,
+        error:
+          error instanceof Error
+            ? error
+            : new Error('An unexpected error occurred'),
+        data: null,
+      });
+    }
   }, []);
 
-  return deleteUserState;
+  return {...deleteUserState, handleDeleteUser};
 };
 
 export default useDeleteUser;

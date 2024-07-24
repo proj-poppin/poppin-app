5d78ad5c2b852e3a1443b09532d369f29dcd0095
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-07-24 17:57:19 +0900

    Fix: 캘린더 뷰에서 발생하는 에러 픽스 #99

--- src/pages/likes/calendar/BottomSheetItem.tsx
+++ src/pages/likes/calendar/BottomSheetItem.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {TouchableOpacity, View} from 'react-native';
 import InterestPopUpCard from '../../../components/molecules/card/InterestPopUpCard.tsx';
 import {GetInterestPopUpListResponse} from '../../../types/PopUpListData.ts';
@@ -7,9 +8,16 @@ import DividerLine from '../../../components/DividerLine.tsx';
 type ItemData = {
   item: GetInterestPopUpListResponse;
   navigation: NavigationProp<any>;
+  showToast: (message: string) => void;
+  isLoggedIn: boolean;
 };
 
-const BottomSheetItem: React.FC<ItemData> = ({item, navigation}) => {
+const BottomSheetItem: React.FC<ItemData> = ({
+  item,
+  navigation,
+  showToast,
+  isLoggedIn,
+}) => {
   return (
     <View
       style={{
@@ -20,7 +28,12 @@ const BottomSheetItem: React.FC<ItemData> = ({item, navigation}) => {
         marginBottom: 2,
       }}>
       <TouchableOpacity
-        onPress={() => navigation.navigate('PopUpDetail', {id: item.id})}>
+        onPress={() =>
+          navigation.navigate('PopUpDetail', {
+            id: item.id,
+            isLoggedIn: isLoggedIn,
+          })
+        }>
         <InterestPopUpCard
           image_url={item.image_url}
           name={item.name}
@@ -28,7 +41,7 @@ const BottomSheetItem: React.FC<ItemData> = ({item, navigation}) => {
           open_date={item.open_date}
           status={item.status}
           id={item.id}
-          isInterested={true}
+          showToast={showToast}
         />
       </TouchableOpacity>
       <DividerLine height={2} />

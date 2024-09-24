3c23d9487c5ff298a75dbcfe499d8cdd153305aa
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-09-24 14:06:42 +0900

    Chore: eslint, tsconfig 수정 #104

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,27 @@
+// module.exports = {
+//   root: true,
+//   extends: '@react-native',
+//   overrides: [
+//     {
+//       files: ['*.ts', '*.tsx'],
+//       rules: {
+//         '@typescript-eslint/no-unused-vars': 'off',
+//         '@typescript-eslint/no-shadow': ['error'],
+//         'no-shadow': 'off',
+//         'no-undef': 'off',
+//       },
+//     },
+//   ],
+// };
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',

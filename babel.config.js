7cd056a81180c1cf78bc52c827ae8fb77fd6e7c4
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-09-30 18:14:13 +0900

    Delete: Font, 불필요한 루트경로내 파일 제거 #104

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'babel-plugin-styled-components',
-    'react-native-reanimated/plugin',
     [
       'module-resolver',
       {

000e8b4f34c22b22ca13bb6cb387be640f7057de
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-11-05 18:56:40 +0900

    Chore: 불필요 부분 주석처리 #104

--- src/hooks/auth/useLogout.tsx
+++ src/hooks/auth/useLogout.tsx
@@ -1,39 +1,39 @@
-import {useState, useCallback} from 'react';
-import {useAppDispatch} from '../../redux/stores';
-import userSlice from '../../redux/slices/user.ts';
-import {resetInterests} from '../../redux/slices/interestSlice.ts';
-import {axiosLogout} from '../../Axios/auth/auth.axios.ts';
-
-const useLogout = () => {
-  const dispatch = useAppDispatch();
-  const [logoutStatus, setLogoutStatus] = useState({
-    success: false,
-    error: null,
-  });
-
-  const handleLogout = useCallback(async () => {
-    try {
-      const result = await axiosLogout();
-      if (result && result.success) {
-        dispatch(
-          userSlice.actions.setAccessTokenAndRefreshToken({
-            accessToken: '',
-            refreshToken: '',
-          }),
-        );
-
-        dispatch(userSlice.actions.resetUser());
-        dispatch(resetInterests());
-        setLogoutStatus({success: true, error: null});
-        return setLogoutStatus({success: true, error: null});
-      }
-    } catch (error) {}
-  }, [dispatch]);
-
-  return {
-    handleLogout,
-    logoutStatus,
-  };
-};
-
-export default useLogout;
+// import {useState, useCallback} from 'react';
+// import {useAppDispatch} from '../../redux/stores';
+// import userSlice from '../../redux/slices/user.ts';
+// import {resetInterests} from '../../redux/slices/interestSlice.ts';
+// import {axiosLogout} from '../../Axios/auth/auth.axios.ts';
+//
+// const useLogout = () => {
+//   const dispatch = useAppDispatch();
+//   const [logoutStatus, setLogoutStatus] = useState({
+//     success: false,
+//     error: null,
+//   });
+//
+//   const handleLogout = useCallback(async () => {
+//     try {
+//       const result = await axiosLogout();
+//       if (result && result.success) {
+//         dispatch(
+//           userSlice.actions.setAccessTokenAndRefreshToken({
+//             accessToken: '',
+//             refreshToken: '',
+//           }),
+//         );
+//
+//         dispatch(userSlice.actions.resetUser());
+//         dispatch(resetInterests());
+//         setLogoutStatus({success: true, error: null});
+//         return setLogoutStatus({success: true, error: null});
+//       }
+//     } catch (error) {}
+//   }, [dispatch]);
+//
+//   return {
+//     handleLogout,
+//     logoutStatus,
+//   };
+// };
+//
+// export default useLogout;

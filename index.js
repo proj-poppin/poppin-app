0d1123d23c6ef33da5ac7017db3b3922cdd33d1f
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-12-21 23:43:50 +0900

    Chore: Add KakaoSDKInit, NaverLoginInit

--- index.js
+++ index.js
@@ -5,7 +5,25 @@
 import {AppRegistry} from 'react-native';
 import {App} from './src/App';
 import messaging from '@react-native-firebase/messaging';
+import {initializeKakaoSDK} from '@react-native-kakao/core';
+import NaverLogin from '@react-native-seoul/naver-login';
+import Config from 'react-native-config';
 import {name as appName} from './app.json';
+
+// #SETTING #@react-native-kakao
+initializeKakaoSDK(`${Config.KAKAO_API_KEY}`);
+
+const consumerKey = Config.NAVER_CONSUMER_KEY;
+const consumerSecret = Config.NAVER_SECRECT_KEY;
+const serviceUrlShemeIOS = Config.NAVER_URL;
+
+NaverLogin.initialize({
+  appName,
+  consumerKey,
+  consumerSecret,
+  serviceUrlSchemeIOS: serviceUrlShemeIOS,
+  disableNaverAppAuthIOS: true,
+});
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {});
 

cb4bf03d10709334d84253eb38cf9288e4ae79cc
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2025-01-13 02:14:31 +0900

    Feat: Fix Component styles

--- src/Component/Image/ProgressBarComponent.tsx
+++ src/Component/Image/ProgressBarComponent.tsx
@@ -1,30 +1,56 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import ProgressFirstSvg from '../../../assets/images/progress1.svg';
-import ProgressSecondSvg from '../../../assets/images/progress2.svg';
-import ProgressThirdSvg from '../../../assets/images/progress3.svg';
+import {FastImageContainer} from './FastImage.component';
+import {Dimensions, StyleProp, ViewStyle, View} from 'react-native';
 
-const ProgressBarComponent = ({step}) => {
+type ProgressBarStepComponentHeaderProps = {
+  step: 1 | 2 | 3; // 단계 값 제한
+  style?: StyleProp<ViewStyle>; // 추가 스타일 속성
+};
+
+export const ProgressBarStepComponentHeader: React.FC<
+  ProgressBarStepComponentHeaderProps
+> = ({step, style}) => {
+  const {width} = Dimensions.get('window');
+
   const renderProgressBar = () => {
+    let source;
     switch (step) {
       case 1:
-        return <ProgressFirstSvg />;
+        source = require('src/Resource/png/progress-bar-step-1.png');
+        break;
       case 2:
-        return <ProgressSecondSvg />;
+        source = require('src/Resource/png/progress-bar-step-2.png');
+        break;
       case 3:
-        return <ProgressThirdSvg />;
+        source = require('src/Resource/png/progress-bar-step-3.png');
+        break;
       default:
         return null;
     }
+
+    return (
+      <FastImageContainer
+        style={[
+          {
+            width: width * 0.8, // 너비를 화면의 80%로 설정
+            height: 10,
+            alignSelf: 'center', // 수평 중앙 정렬
+          },
+          style, // 커스텀 스타일 병합
+        ]}
+        source={source}
+      />
+    );
   };
 
-  return <View style={styles.container}>{renderProgressBar()}</View>;
+  return (
+    <View
+      style={{
+        justifyContent: 'center', // 세로 중앙 정렬
+        alignItems: 'center', // 가로 중앙 정렬
+        width: '100%', // 부모 컨테이너 너비
+      }}>
+      {renderProgressBar()}
+    </View>
+  );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center', // 가로 축에서 중앙 정렬
-  },
-});
-
-export default ProgressBarComponent;

c9c6190bd92c66ceba69b3f04a7696eeb992f6eb
kyujenius <hkj020607@gmail.com>
2025-01-17 20:43:56 +0900

    feat: axios 비로그인시 403 에러 헨들러 구현 및 적용

--- src/Util/axios.util.ts
+++ src/Util/axios.util.ts
@@ -45,3 +45,56 @@ export async function handleAxiosError(param: {
   });
   return;
 }
+
+
+/**
+ * Axios 요청 중 에러가 났을 때 status가 403일 때,
+ * 로그인이 안 되어있는지 확인하고, 공통으로 사용할 에러를 지정해줍니다.
+ * @author 규진
+ */
+export async function handleAuthCheckAxiosError(param: {
+  error: AxiosError<ResponseWrapper> | any;
+  errorMessage?: string;
+  toastShowParams?: ToastShowParams;
+}) {
+  //* 발생한 에러가 AxiosError인지 검증하고 response 데이터를 가져올 수 있는지 확인합니다.
+  if (axios.isAxiosError(param.error)) {
+    const responseData = param.error.response?.data as ResponseWrapper;
+    const statusCode = param.error.response?.status;
+
+    //* 403 상태 코드 처리 (인증 관련 에러)
+    if (statusCode === 403) {
+      showBlackToast({
+        ...param.toastShowParams,
+        text1: '로그인이 필요한 서비스입니다. 로그인 후 다시 시도해 주세요.',
+      });
+      return;
+    }
+
+    //* 서버에서 code가 '500'인 경우 처리
+    if (responseData?.error?.code === '500') {
+      showBlackToast({
+        ...param.toastShowParams,
+        text1:
+            '서버 에러입니다. 잠시 후 다시 시도해 주세요.\n문제가 지속되는 경우, 고객센터를 통해 문의해 주세요',
+      });
+      return;
+    }
+
+    //* 서버에서 ResponseWrapper.error 필드로 에러 메시지를 보내온 경우
+    if (responseData?.error?.message) {
+      showBlackToast({
+        ...param.toastShowParams,
+        text1: responseData.error.message,
+      });
+      return;
+    }
+  }
+
+  //* AxiosError가 아니거나 서버에서 에러 메시지를 보내지 않은 경우, 미리 지정한 메세지를 toast로 띄웁니다.
+  showBlackToast({
+    ...param.toastShowParams,
+    text1: param.errorMessage || '알 수 없는 오류가 발생했습니다.',
+  });
+  return;
+}

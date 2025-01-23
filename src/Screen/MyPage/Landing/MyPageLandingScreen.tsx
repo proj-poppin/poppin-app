6f90a2eef7af5118d3bc81309bc35b59d79ae8f4
kyujenius <hkj020607@gmail.com>
2025-01-24 03:07:09 +0900

    Delete: 불필요 코드들 삭제

--- src/Screen/MyPage/Landing/MyPageLandingScreen.tsx
+++ src/Screen/MyPage/Landing/MyPageLandingScreen.tsx
@@ -1,8 +1,6 @@
-import {ScrollView} from 'react-native';
-import React, {useCallback} from 'react';
+import React from 'react';
 import styled from 'styled-components/native';
 import {Screen} from '../../../Component/Screen/Screen.component';
-import {SectionContainer} from '../../../Unit/View';
 import {MyPageLandingScreenHeader} from './Mypage.landing.screenHeader';
 import {MyPageLandingProfileSection} from './Section/Mypage.landing.profile.section';
 import {MyPageLandingReviewSection} from './Section/MyPage.landing.review.section';
@@ -15,21 +13,6 @@ import {MypageLandingLogoutModal} from './Mypage.landing.logout.modal';
 export type MyPageLandingScreenProps = {};
 
 export const MyPageLandingScreen = () => {
-  /**
-   * 뒤로 가기 버튼을 누르면 홈 랜딩 페이지로 이동합니다.
-   * @author 도형
-   */
-
-  const [isVisible, setIsVisible] = React.useState(false);
-
-  const pressReportButtonHandler = useCallback(() => {
-    setIsVisible(true);
-  }, []);
-
-  const handleClose = useCallback(() => {
-    setIsVisible(false);
-  }, []);
-
   return (
     <Screen
       contentContainerStyle={{
@@ -52,54 +35,8 @@ export const MyPageLandingScreen = () => {
       }
     />
   );
-
-  // return (
-  //   <Container>
-  //     {/*<Screen*/}
-  //     {/*  contentContainerStyle={{*/}
-  //     {/*    paddingTop: moderateScale(16),*/}
-  //     {/*    paddingBottom: moderateScale(80),*/}
-  //     {/*  }}*/}
-  //     {/*  ScreenHeader={<MypageLandingScreenHeader />}*/}
-  //     {/*  ScreenContent={*/}
-  //     {/*    <>*/}
-  //     {/*      <MypageLandingProfile />*/}
-  //     {/*      <MypageLandingPartnerCreditSection />*/}
-  //     {/*      <MypageLandingCreditSection />*/}
-  //     {/*      <MypageLandingNoticeBannerSection />*/}
-  //     {/*      <MypageLandingMyActivitySection />*/}
-  //     {/*      <MypageLandingQuickMenuSection />*/}
-  //     {/*      <MypageLandingFunctionSection />*/}
-  //     {/*      /!* <MypageLandingEvent /> *!/*/}
-  //     {/*      /!* <MypageLandingFollow /> *!/*/}
-  //     {/*      <MypageLandingAdBannerSection />*/}
-  //     {/*      <MypageLandingAboutSection />*/}
-  //     {/*    </>*/}
-  //     {/*  }*/}
-  //     {/*/>*/}
-  //     {/*기획 다시 나올 때까지 주석처리 */}
-  //     {/* <MyPageLandingCreateIconSection /> */}
-  //   </Container>
-  // );
 };
 
 const MyPagePaddingSection = styled.View`
-  padding: ${moderateScale(4)}px ${moderateScale(12)}px;
+  padding: ${moderateScale(12)}px ${moderateScale(12)}px ${moderateScale(32)}px;
 `;
-
-const Container = styled.View`
-  position: relative;
-  flex: 1;
-`;
-
-/**
- * 마이페이지 랜딩 페이지에서만 사용되는 스타일
- * @author 도형
- */
-// export const mypageLandingScreenStyles = StyleSheet.crate({
-//   /** 섹션 구분선 */
-//   boundary: {
-//     borderBottomWidth: 6,
-//     borderBottomColor: themeColors().purple.mild,
-//   },
-// });
